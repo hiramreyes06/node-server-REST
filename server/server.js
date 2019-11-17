652339c1a5d76eef70231e254d0e6795be5b4b89
hiramreyes06 <luishiramreyes@hotmail.com>
2019-11-17 14:06:57 -0600

    acces origin

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ const bodyParser = require('body-parser')
 //Y para obtener el payload que tiene la peticion post 
 //Cada peticion pasa por estas lineas debodyparser
 
+
+//Permitir posteos y sus de la api desde cualquier lugar
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
