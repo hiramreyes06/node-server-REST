5f57373421d4f9794e1f9cc524424fb3bb98ffcd
hiramreyes06 <luishiramreyes@hotmail.com>
2019-12-14 12:06:15 -0600

    nmss

--- server/server.js
+++ server/server.js
@@ -18,16 +18,9 @@ const bodyParser = require('body-parser')
 
 
 //Permitir uso y acceso a la api desde cualquier lugar
-//app.use(cors({ origin :'*' , credentials:true }));
-
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+app.use(cors({ origin :'*' , credentials:true,  }));
 
 
-  next();
-});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
